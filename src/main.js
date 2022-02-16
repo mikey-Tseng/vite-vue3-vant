8d11b996463179d737eb92e7eda04bb615de2b26
Mikey <zxiangyun@sph.com.sg>
2022-02-16 16:47:49 +0800

    add common.css

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import { useComponents } from './plugins/components'
 import './plugins/theme.css'
+import './plugins/common.css'
 const app = createApp(App)
 useComponents(app).use(router).use(store).mount('#app')
